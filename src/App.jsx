b7d7339f793bae9eefd11217871d738c641588a4
Ojas Bhalerao <meerasbhalerao@gmail.com>
2024-07-12 22:03:53 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import "./App.css";
+import Btngrp from "./btn-group.jsx";
+function App() {
+  return (
+    <>
+      <Btngrp />
+    </>
+  );
+}
+
+export default App;
---
+++ src/btn-group.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+import "./btngrp.css";
+export default function Btngrp() {
+  const [count, setcount] = useState("");
+
+  let values = ["%", "X", "-", "+", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "."];
+
+  let clicked = (event) => {
+    let target = event.target.value;
+    return setcount((prevcount) => prevcount + target);
+  };
+
+  let equ = () => {
+    return setcount((val) => eval(val));
+  };
+
+  let reset = () => {
+    return setcount("");
+  };
+
+  return (
+    <div className="container">
+      <div>
+        <span>
+          {" "}
+          <input
+            className="display-input"
+            type="text"
+            placeholder=""
+            value={count}
+            readOnly
+          />
+        </span>
+      </div>
+      <div className="test">
+        {values.map((symbol) => (
+          <button
+            className="btns"
+            id={symbol}
+            onClick={clicked}
+            key={symbol}
+            value={symbol}
+          >
+            {symbol}
+          </button>
+        ))}
+        <button className="btns" onClick={reset}>
+          Ac
+        </button>
+        <button className="equals" onClick={equ}>
+          =
+        </button>
+      </div>
+    </div>
+  );
+}
